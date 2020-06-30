33f7cadd70bbb18f874f744797b320f8f7dc2f6d
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-06-30 20:09:58 -0300

    refactor(challenge): remove commend from bash code

--- challenges/02/run.sh
+++ challenges/02/run.sh
@@ -4,5 +4,8 @@ buffalo dev
 # Builds docker image
 docker build -t guilhermebkel/fullcyclebootcamp:hello-buffalo .
 
+# Runs the built docker image
+docker run -p 3000:3000 guilhermebkel/fullcyclebootcamp:hello-buffalo
+
 # Pushes docker image to DockerHub
 docker push guilhermebkel/fullcyclebootcamp:hello-buffalo
