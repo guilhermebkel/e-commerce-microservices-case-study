edb0128eb9f7a8fd92a9380de0ab2c3f0717cc47
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-07-05 15:20:32 -0300

    feat(challenge): add docker commands

--- challenges/05/run.sh
+++ challenges/05/run.sh
@@ -6,3 +6,9 @@ docker run -p 3000:3000 guilhermebkel/fullcyclebootcamp-nest:hello-nest
 
 # Pushes docker image to DockerHub
 docker push guilhermebkel/fullcyclebootcamp-nest:hello-nest
+
+# Pushes as latest
+docker tag guilhermebkel/fullcyclebootcamp-nest:hello-nest guilhermebkel/fullcyclebootcamp-nest:latest
+
+# Pushes latest layer
+docker push guilhermebkel/fullcyclebootcamp-nest:latest
